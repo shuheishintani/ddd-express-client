1822ea8637dbb10651f3b870e1bca5a8d604d4d9
shuheishintani <shuheishintani.dev@gmail.com>
2021-03-27 14:07:22 +0900

    cors対応

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -8,6 +8,7 @@ export const useAxios = () => {
   const { setUser } = useContext(AuthContext);
 
   axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+  axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
   axios.defaults.withCredentials = true;
   axios.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
